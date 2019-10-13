ce2d98dc8e9a3233f39502cbd150687b8f254bc8
MorrisMorrison <mwlltrdev@gmail.com>
2019-10-14 01:25:16 +0200

    add basic service implementation

---
+++ SimpleRestFramework/SimpleRestFramework/Service/CrudService.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using SimpleRestFramework.Persistence;
+
+namespace SimpleRestFramework.Service
+{
+
+    public interface ICrudService<T> where T:IEntity
+    {
+        IDao<T> Dao { get; set; }
+
+        T Get(long p_id);
+        void Create(T p_entity);
+        void Update(T p_entity);
+        void Delete(T p_entity);
+
+    }
+    
+    public abstract class AbstractCrudService<T>:ICrudService<T>  where T:IEntity
+    {
+        public IDao<T> Dao { get; set; }
+
+        protected AbstractCrudService(IDao<T> p_dao)
+        {
+            Dao = p_dao;
+        }
+
+        public T Get(long p_id)
+        {
+            return Dao.Get(p_id);
+        }
+
+        public void Create(T p_entity)
+        {
+            Dao.Create(p_entity);
+        }
+
+        public void Update(T p_entity)
+        {
+            Dao.Update(p_entity);
+        }
+
+        public void Delete(T p_entity)
+        {
+            Dao.Delete(p_entity);
+        }
+    }
+}
